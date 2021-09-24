be5bb6237dafc6c4514ff50640cf126cf60a03bc
Israel <israeladefidipe@gmail.com>
2021-09-24 05:57:16 +0100

    header completed for desktop

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
-import {StyledHeader} from './Styles/Header.styled'
+import { Container } from './Styles/Container.styled'
+import {StyledHeader, Nav, Logo} from './Styles/Header.styled'
+import logo from '../images/logo.svg'
+import { Button } from './Styles/Button.styled'
 
 export default function Header() {
     return (
         <StyledHeader>
-            <h1>Huddle</h1>
+            <Container>
+                <Nav>
+                    <Logo src={logo} />
+                    <Button> Try It Free </Button>
+
+                </Nav>
+            </Container>
           
         </StyledHeader>
     )
